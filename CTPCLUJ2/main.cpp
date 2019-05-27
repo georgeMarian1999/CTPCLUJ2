2387dcaf9571365ec44705e1a2901613894460aa
Marian George <mariangeorge99@icloud.com>
2019-05-27 15:00:44 +0300

    Initial Commit

---
+++ CTPCLUJ2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CTPCLUJ2
+//
+//  Created by Marian George on 27/05/2019.
+//  Copyright © 2019 Marian George. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
