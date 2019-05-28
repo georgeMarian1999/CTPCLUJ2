cb9d79353969cb055d82fc8ae8aed2f4b6aea8b1
alexmazarache <44412246+alexmazarache@users.noreply.github.com>
2019-05-28 18:26:49 +0300

    bazadateclienti

--- CTPCLUJ2/BazaDateClienti.cpp
+++ CTPCLUJ2/BazaDateClienti.cpp
@@ -15,106 +15,100 @@ using namespace std;
 #include "RepoBilete.h"
 #include "Card.h"
 #include <string>
+#include "ClientLogat.h"
 
-BazaDateClienti::BazaDateClienti(){
+BazaDateClienti::BazaDateClienti() {
 
 	this->elements.reserve(10);
 }
 
 
-BazaDateClienti::BazaDateClienti(const BazaDateClienti &r){
-	this->elements=r.elements;
+BazaDateClienti::BazaDateClienti(const BazaDateClienti &r) {
+	this->elements = r.elements;
 }
 
 
-BazaDateClienti::~BazaDateClienti(){
+BazaDateClienti::~BazaDateClienti() {
 	elements.clear();
 }
 
 
-BazaDateClienti& BazaDateClienti::operator=(const BazaDateClienti &r){
-	if (this != &r){
+BazaDateClienti& BazaDateClienti::operator=(const BazaDateClienti &r) {
+	if (this != &r) {
 		elements.clear();
 		elements.reserve(r.elements.size());
-		for (int i = 0; i < r.elements.size(); i++)
+		for (unsigned int i = 0; i < r.elements.size(); i++)
 			elements[i] = r.elements[i];
 	}
 	return *this;
 }
 
 
-int BazaDateClienti::getSize(){
-	return int(elements.size());
+int BazaDateClienti::getSize() {
+	return elements.size();
 }
 
 
-vector<Client*> BazaDateClienti::getAll(){
+vector<Client*> BazaDateClienti::getAll() {
 	//if(pos<0 || pos>=elements.size())
 //		throw Exception("pozitia trebuie sa fie intre 0 si dimensiunea vectorului...");
 	return this->elements;
 }
 
 
-void BazaDateClienti::addClient(Client* el){
+void BazaDateClienti::addClient(Client* el) {
 
 	elements.push_back(el);
 }
 
 
-void BazaDateClienti::update(Client* s, int pos){
+void BazaDateClienti::update(Client* s, int pos) {
 	/*Descr:update pe pozitia pos cu obiectul s
 	 * In:obiectul s, pozitia pos
 	 * Out:-
 	 */
-	//if(pos<0 || pos>=elements.size())
-	//	throw Exception("pozitia trebuie sa fie intre 0 si dimensiunea vectorului...");
-	this->elements[pos]=s;
+	 //if(pos<0 || pos>=elements.size())
+	 //	throw Exception("pozitia trebuie sa fie intre 0 si dimensiunea vectorului...");
+	this->elements[pos] = s;
 }
 void BazaDateClienti::LoadFromFile(const char* filename)
 {
-	ifstream f(filename);
-	while(!f.eof())
+
+
+	ifstream f("BazaDateClienti.txt");
+	while (!f.eof())
 	{
-		char* string=new char[100];
-		int nrcard,nrpin;
-		char* username=new char[100];
-		char* parola=new char[100];
+		char* string = new char[100];
+		char*nrcard = new char[100];
+		char* nrpin = new char[100];
+		char* username = new char[100];
+		char* parola = new char[100];
 		int pret, zona;
-	
-		f.getline(string,100);
-		char* p=strtok(string,",");
-		nrcard=atoi(p);
-		p=strtok(NULL,",");
-		nrpin=atoi(p);
-		p=strtok(NULL,",");
-		strcpy(username,p);
-		p=strtok(NULL,",");
-		strcpy(parola,p);
-		p=strtok(NULL,",");
-		Card cd(nrcard,nrpin);
+
+		f.getline(string, 100);
+		int Nrcard = atoi(strtok_s(string, ",", &nrpin));
+		int Nrpin = atoi(strtok_s(nrpin, ",", &username));
+		username = strtok_s(username, ",", &parola);
+		parola = strtok_s(parola, ",", &nrcard);
+		
+		Card cd(Nrcard, Nrpin);
 		RepoBilete r;
-		if(p)
+		
+		if (nrcard)
 		{
-			while(p)
-			{
-				pret=atoi(p);
-				p=strtok(NULL,",");
-				zona=atoi(p);
-				p=strtok(NULL,",");
-				Bilet b(pret,zona);
-				r.addElement(b);
-			}
+			pret = nrcard[0] - '0';
+			zona = nrcard[2] - '0';
+			r.addElement(Bilet(pret, zona));
 		}
 		
-		Client* c=new ClientLogat(cd,username,parola,r);
-		this->addClient(c); 
+		Client* c = new ClientLogat(cd, username, parola, r);
+		this->addClient(c);
+		
 
-
-		f.close();
-
-
+		cout << c->toString();
 
 	}
+	f.close();
 }
 
 
