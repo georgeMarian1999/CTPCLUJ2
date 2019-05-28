dddb1e3232d80d52c04ea438ab07d7a57e14af80
Marian George <mariangeorge99@icloud.com>
2019-05-28 17:30:54 +0300

    Update Tests.hpp

--- CTPCLUJ2/Tests.hpp
+++ CTPCLUJ2/Tests.hpp
@@ -13,13 +13,13 @@
 #include "Orar.hpp"
 #include "Statie.hpp"
 #include "Ora.hpp"
-#include"Bilet.h"
-#include"Card.h"
-#include"Client.h"
+#include "Bilet.h"
+#include "Card.h"
+#include "Client.h"
 #include "Autobuz.h"
-#include"BazaDateClienti.h"
+#include "BazaDateClienti.h"
 #include "ClientLogat.hpp"
-
+#include "BazaDateAutobuze.h"
 using namespace std;
 class Tests{
 public:
@@ -33,6 +33,7 @@ public:
 	bool test_Autobuz();
 	bool test_Clientlogat();
 	bool test_BazaDateClienti();
+    bool test_BazaDateAutobuze();
     void RunAllTests();
 };
 #endif /* Tests_hpp */
