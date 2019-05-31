de9efc4c66643115c454c9a4cfad9da972b9b99e
Marian George <mariangeorge99@icloud.com>
2019-05-31 16:54:49 +0300

    Djsajsfajfrcc

--- CTPCLUJ2/Autobuz.cpp
+++ CTPCLUJ2/Autobuz.cpp
@@ -3,15 +3,18 @@
 
 Autobuz::Autobuz()
 {
+    //constructor
     numar=0;
     Orar orar;
 }
 Autobuz::Autobuz(const Autobuz& a)
 {
+    //constructor cu parametrii
 	this->numar = a.numar;
     this->orar=a.orar;
 }
 Autobuz& Autobuz::operator=(const Autobuz &a){
+    //constructor de atribuire
     if(this!=&a){
         this->numar = a.numar;
         this->orar=a.orar;
@@ -20,24 +23,31 @@ Autobuz& Autobuz::operator=(const Autobuz &a){
 }
 Autobuz::Autobuz(int nr,Orar r)
 {
+    //constructor de atribuire
 	this->numar=nr;
     this->orar.setMatrice(r.getTimpi());
     this->orar.setStatii(r.getStatii());
 }
 Autobuz::~Autobuz()
 {
+    //destructor
 }
 bool Autobuz::operator==(const Autobuz &A){
+    //operator egal
     if(numar!=A.numar)
         return false;
     return orar==A.orar;
 }
 bool Autobuz::operator!=(const Autobuz&A){
+    //operator diferit
     if(numar==A.numar)
         return false;
     return orar!=A.orar;
 }
 string Autobuz::toString(){
+    //Descriere:functie care stocheaza intr un string forma cum se va afisa autobuzul
+    //input:--
+    //output:un string 
     string s;
     s=s+"Autobuzul cu numarul "+to_string(numar)+" are orarul:\n";
     s=s+orar.toString();
