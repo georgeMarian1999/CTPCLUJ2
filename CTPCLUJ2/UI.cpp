05b7908231b665ad08a9842c5673aa94f6893bcc
Marian George <mariangeorge99@icloud.com>
2019-06-01 13:07:47 +0300

    Update UI.cpp

--- CTPCLUJ2/UI.cpp
+++ CTPCLUJ2/UI.cpp
@@ -1,6 +1,25 @@
 #include "UI.h"
 #include <string>
-#include <Windows.h>
+#include <windows.h>
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
+#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
+#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
+#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
+#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
+#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
+#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+
+//#include <Windows.h>
 UI::UI(){
     
 }
@@ -30,7 +49,7 @@ void UI::filtrare_nr()
         std::cout<<'\n';
         vector<Autobuz> aux;
         aux=this->c.filterByNr(nr);
-        std::cout<<aux[0].toString()<<'\n';
+        std::cout<< MAGENTA <<aux[0].toString()<< RESET <<'\n';
 
 }
 void UI::filtrare_statie()
